fd9f7623e444d6419b31f6740bbbd2d69dfd667d
Tommysons <lokatorist2@inbox.lv>
2024-09-24 07:47:41 -0700

    Test2

---
+++ app/index.js
@@ -0,0 +1,7 @@
+export const runtime = 'edge';
+
+const HomePage = () => {
+    return <div>Welcome to My App!</div>;
+};
+
+export default HomePage;
---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export const runtime = 'edge';
+
+const NotFound = () => {
+    return <div>Page Not Found</div>;
+};
+
+export default NotFound;
