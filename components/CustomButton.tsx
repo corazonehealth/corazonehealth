bdff405db3b19c1a33ba7435d3719e97c3227bff
Tommysons <lokatorist2@inbox.lv>
2026-01-21 23:38:02 -0800

    sse

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,20 @@
-import { motion } from "framer-motion"
-import { fadeIn } from "@/lib/variants"
-import React from "react";
-import { useRouter } from "next/navigation";
+'use client'
+import { motion } from 'framer-motion'
+import { fadeIn } from '@/lib/variants'
+import React from 'react'
+import { useRouter } from 'next/navigation'
 
 interface CustomButtonProps {
-  text: string;
-  containerStyles: string;
+  text: string
+  containerStyles: string
   href?: string
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ text, containerStyles, href }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  containerStyles,
+  href,
+}) => {
   const router = useRouter()
   const handleClick = () => {
     if (href) {
@@ -17,17 +22,19 @@ const CustomButton: React.FC<CustomButtonProps> = ({ text, containerStyles, href
     }
   }
   return (
-    <motion.button 
-    variants={fadeIn('up', 1)}
-    initial="hidden"
-    whileInView={'show'}
-    viewport={{ once: false, amount: 0.2 }}
-    className={`${containerStyles} group relative cursor-pointer overflow-hidden bg-accent uppercase rounded-lg`}
-    onClick={handleClick}
+    <motion.button
+      variants={fadeIn('up', 1)}
+      initial='hidden'
+      whileInView={'show'}
+      viewport={{ once: false, amount: 0.2 }}
+      className={`${containerStyles} group relative cursor-pointer overflow-hidden bg-accent uppercase rounded-lg`}
+      onClick={handleClick}
     >
-      <span className="ease absolute top-1/2 h-0 w-64 origin-center -translate-x-20 rotate-45 bg-black transition-all
-      duration-300 group-hover:h-64 group-hover:-translate-y-32"></span>
-      <span className=" ease relative text-white transition duration-300 group-hover:text-white">
+      <span
+        className='ease absolute top-1/2 h-0 w-64 origin-center -translate-x-20 rotate-45 bg-black transition-all
+      duration-300 group-hover:h-64 group-hover:-translate-y-32'
+      ></span>
+      <span className=' ease relative text-white transition duration-300 group-hover:text-white'>
         {text}
       </span>
     </motion.button>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,82 +1,69 @@
-"use client";
+'use client'
 
-import React, { useEffect, useState } from 'react';
-import Navbar from './Navbar';
-import MobileNav from './MobileNav';
-import Link from 'next/link';
-import Image from 'next/image';
-import { MdMenu } from 'react-icons/md';
-import { SignedIn, SignedOut, UserButton, SignInButton, useAuth } from '@clerk/nextjs';
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+import MobileNav from './MobileNav'
+import Link from 'next/link'
+import Image from 'next/image'
+import { MdMenu } from 'react-icons/md'
 
 const Header = () => {
-  const [headerActive, setHeaderActive] = useState(false);
-  const [openNav, setOpenNav] = useState(false);
-  const [isClient, setIsClient] = useState(false); // Add a state to check if we're on the client
-  const { isLoaded, isSignedIn } = useAuth();
+  const [headerActive, setHeaderActive] = useState(false)
+  const [openNav, setOpenNav] = useState(false)
 
   useEffect(() => {
-    // This will ensure we are running on the client side
-    setIsClient(true);
-
     const handleScroll = () => {
-      setHeaderActive(window.scrollY > 50);
-    };
+      setHeaderActive(window.scrollY > 50)
+    }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  if (!isLoaded) {
-    return (
-      <header className="fixed max-w-[1920px] top-0 w-full bg-primary-200 transition-all z-50 h-[140px]">
-        <div className='container mx-auto flex items-center justify-between h-full'>
-          <div className="animate-pulse h-10 w-40 bg-gray-300" />
-        </div>
-      </header>
-    );
-  }
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
-    <header className={`${headerActive ? 'h-[100px]' : 'h-[140px]'} 
-      fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[133px] transition-all z-50`}>
+    <header
+      className={`${headerActive ? 'h-[100px]' : 'h-[140px]'} 
+      fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[133px] transition-all z-50`}
+    >
       <div className='container mx-auto flex items-center justify-between'>
         <Link href='/'>
-          <Image className='mt-2' src={'/assets/logo.jpg'} width={150} height={150} alt='logo'/>
+          <Image
+            className='mt-2'
+            src={'/assets/logo.jpg'}
+            width={150}
+            height={150}
+            alt='logo'
+          />
         </Link>
 
-        <MobileNav 
+        <MobileNav
           containerStyles={`${headerActive ? 'top-[124px]' : 'top-[124px]'} 
-          ${openNav ? "max-h-max pt-8 pb-10 border-t border-white/10" :
-             "max-h-0 pt-0 pb-0 overflow-hidden border-white/0"}
+          ${
+            openNav
+              ? 'max-h-max pt-8 pb-10 border-t border-white/10'
+              : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'
+          }
           flex flex-col text-center gap-8 
           fixed bg-primary-200 w-full left-0 text-base uppercase
           font-medium text-white transition-all xl:hidden`}
-          setOpenNav={setOpenNav} // Pass setOpenNav as a prop
+          setOpenNav={setOpenNav}
         />
 
-        <Navbar containerStyles='flex gap-4 text-white text-base uppercase font-medium transition-all hidden xl:flex'/>
+        <Navbar containerStyles='flex gap-4 text-white text-base uppercase font-medium transition-all hidden xl:flex' />
 
         <div className='flex items-center gap-4 text-white text-base uppercase font-medium'>
-          {/* {isClient && isSignedIn ? ( // Ensure this runs only on the client
-            <UserButton />
-          ) : (
-            isClient && (
-              <SignedOut>
-                <SignInButton>
-                  Sign In
-                </SignInButton>
-              </SignedOut>
-            )
-          )} */}
-          <button onClick={() => setOpenNav(!openNav)} className='text-white xl:hidden'>
+          <button
+            onClick={() => setOpenNav(!openNav)}
+            className='text-white xl:hidden'
+          >
             <MdMenu className='text-4xl' />
           </button>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
